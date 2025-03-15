3eeb5b6ddbd7bfa803e09f20de444730540d34c3
andrii_solomka <andriissolomka@gmail.com>
2025-03-15 19:36:49 +0200

    Add Node

--- src/linked-list/linked-list.service.ts
+++ src/linked-list/linked-list.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
+class Node {
+  value: string;
+  next: Node | null = null;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+}
+
 @Injectable()
 export class LinkedListService {}
